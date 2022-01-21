c02106f5997ff15b73b85aff5c305854c4b09d2f
전민종 <giml790880@gmail.com>
2022-01-21 19:20:31 +0900

    3

--- TextRPG.cpp
+++ TextRPG.cpp
@@ -2,7 +2,16 @@
 #include<time.h>
 using namespace std;
 
-enum MAIN_MENU {
+
+// 숙제
+//1. 무기상점에서 판매할 아이템 목록을 만들어주고 해당 목록을 출력해준다.
+//2. 출력할때 앞에 번호를 부여해주고 번호를 입력받아서 해당 번호의 아이템을
+//구입하게 해준다. 가장 마지막 번호에 뒤로가기 기능을 만든다.
+//단, 가방이 꽉 찼거나 돈이 부족할 경우 구매하지 못하게 한다.
+//3. 가방을 선택하면 가방안의 아이템을 보여준다.
+
+enum MAIN_MENU 
+{
 	MM_NONE,
 	MM_MAP,
 	MM_STORE,
@@ -10,7 +19,8 @@ enum MAIN_MENU {
 	MM_EXIT
 };
 
-enum MAP_TYPE {
+enum MAP_TYPE 
+{
 	MT_NONE,
 	MT_EASY,
 	MT_NORMAL,
@@ -18,7 +28,8 @@ enum MAP_TYPE {
 	MT_BACK
 };
 
-enum JOB {
+enum JOB 
+{
 	JOB_NONE,
 	JOB_KNIGHT,
 	JOB_ARCHER,
@@ -26,16 +37,51 @@ enum JOB {
 	JOB_END
 };
 
-enum BATTLE {
+enum BATTLE 
+{
 	BATTLE_NONE,
 	BATTLE_ATTACK,
 	BATTLE_BACK
 };
 
-#define NAME_SIZE	32
+enum ITEM_TYPE
+{
+	IT_NONE,
+	IT_WEAPON,
+	IT_ARMOR,
+	IT_BACK
+};
 
-struct _tagInventory {
-	int		iGold;
+enum STORE_MENU
+{
+	SM_NONE,
+	SM_WEAPON,
+	SM_ARMOR,
+	SM_BACK
+};
+
+#define NAME_SIZE	32
+#define ITEM_DESC_LENGTH	512
+#define INVENROTY_MAX		20
+#define STORE_WEAPON_MAX	3
+#define STORE_ARMOR_MAX		3
+struct _tagItem
+{
+	char	strName[NAME_SIZE];
+	char	strTypeName[NAME_SIZE];
+	ITEM_TYPE	eType;
+	int		iMin;
+	int		iMax;
+	int		iPrice;
+	int		iSell;
+	char	strDesc[ITEM_DESC_LENGTH];	// 아이템 설명
+};
+
+struct _tagInventory 
+{
+	_tagItem	tItem[INVENROTY_MAX];
+	int			iCount;
+	int			iGold;
 };
 struct _tagPlayer
 {
@@ -55,7 +101,8 @@ struct _tagPlayer
 	_tagInventory	tInventory;
 };
 
-struct _tagMonster {
+struct _tagMonster 
+{
 	char	strName[NAME_SIZE];
 	int		iAttackMin;
 	int		iAttackMax;
@@ -80,7 +127,8 @@ int main() {
 	cout << "이름 : ";
 	cin.getline(tPlayer.strName, NAME_SIZE - 1);
 	int iJob = JOB_NONE;
-	while (iJob == JOB_NONE) {
+	while (iJob == JOB_NONE) 
+	{
 		system("cls");
 		cout << "1. 기사" << endl;
 		cout << "2. 궁수" << endl;
@@ -88,13 +136,15 @@ int main() {
 		cout << "직업을 선택하세요 : ";
 		cin >> iJob;
 
-		if (cin.fail()) {
+		if (cin.fail()) 
+		{
 			cin.clear();
 			cin.ignore(1024, '\n');
 			continue;
 		}
 
-		else if (iJob <= JOB_NONE || iJob >= JOB_END) {
+		else if (iJob <= JOB_NONE || iJob >= JOB_END) 
+		{
 			iJob = JOB_NONE;
 		}
 
@@ -102,7 +152,8 @@ int main() {
 		tPlayer.iExp = 0;
 		tPlayer.eJob = (JOB)iJob;
 		tPlayer.tInventory.iGold = 10000;
-		switch (tPlayer.eJob) {
+		switch (tPlayer.eJob) 
+		{
 		case JOB_KNIGHT:
 			strcpy_s(tPlayer.strJobName, "기사");
 			tPlayer.iAttackMax = 5;
@@ -143,21 +194,21 @@ int main() {
 
 	// 고블린 생성
 	strcpy_s(tMonsterArr[0].strName, "고블린");
-	tMonsterArr[0].iAttackMin = 80;
-	tMonsterArr[0].iAttackMax = 130;
-	tMonsterArr[0].iArmorMin = 60;
-	tMonsterArr[0].iArmorMax = 90;
-	tMonsterArr[0].iHP = 2000;
-	tMonsterArr[0].iHPMAX = 2000;
-	tMonsterArr[0].iMP = 100;
-	tMonsterArr[0].iMPMAX = 100;
-	tMonsterArr[0].iLevel = 5;
-	tMonsterArr[0].iExp = 7000;
-	tMonsterArr[0].iGoldMin = 6000;
-	tMonsterArr[0].iGoldMax = 8000;
+	tMonsterArr[0].iAttackMin = 20;
+	tMonsterArr[0].iAttackMax = 30;
+	tMonsterArr[0].iArmorMin = 2;
+	tMonsterArr[0].iArmorMax = 5;
+	tMonsterArr[0].iHP = 100;
+	tMonsterArr[0].iHPMAX = 100;
+	tMonsterArr[0].iMP = 10;
+	tMonsterArr[0].iMPMAX = 10;
+	tMonsterArr[0].iLevel = 1;
+	tMonsterArr[0].iExp = 1000;
+	tMonsterArr[0].iGoldMin = 500;
+	tMonsterArr[0].iGoldMax = 1500;
 
 	// 트롤
-	strcpy_s(tMonsterArr[1].strName, "고블린");
+	strcpy_s(tMonsterArr[1].strName, "트롤");
 	tMonsterArr[1].iAttackMin = 80;
 	tMonsterArr[1].iAttackMax = 130;
 	tMonsterArr[1].iArmorMin = 60;
@@ -185,7 +236,14 @@ int main() {
 	tMonsterArr[2].iGoldMin = 20000;
 	tMonsterArr[2].iGoldMax = 50000;
 
-	while (true) {
+	// 상점에서 판매할 아이템 목록을 생성한다.
+	_tagItem	tStoreWeapon[STORE_WEAPON_MAX] = {};
+	_tagItem	tStoreArmor[STORE_ARMOR_MAX] = {};
+
+	// 각 아이템 정보들을 설정해준다.
+	
+	while (true) 
+	{
 		system("cls");
 		cout << "*********************** 로비 ***********************" << endl;
 		cout << "1. 맵" << endl;
@@ -196,19 +254,23 @@ int main() {
 		int iMenu;
 		cin >> iMenu;
 
-		if (cin.fail()) {
+		if (cin.fail()) 
+		{
 			cin.clear();
 			cin.ignore(1024, '\n');
 			continue;
 		}
 
-		if (iMenu == MM_EXIT) {
+		if (iMenu == MM_EXIT) 
+		{
 			break;
 		}
 
-		switch (iMenu) {
+		switch (iMenu) 
+		{
 		case MM_MAP:
-			while (true) {
+			while (true) 
+			{
 				system("cls");
 				cout << "*********************** 맵 ***********************" << endl;
 				cout << "1. 쉬움" << endl;
@@ -218,7 +280,8 @@ int main() {
 				cout << "맵을 선택하세요 : ";
 				cin >> iMenu;
 
-				if (cin.fail()) {
+				if (cin.fail()) 
+				{
 					cin.clear();
 					cin.ignore(1024, '\n');
 					continue;
@@ -233,8 +296,11 @@ int main() {
 				// 그렇게 해서 해당 맵의 몬스터를 생성해준다.
 				_tagMonster tMonster = tMonsterArr[iMenu - 1];
 
-				while (true) {
-					switch (iMenu) {
+				while (true) 
+				{
+					system("cls");
+					switch (iMenu) 
+					{
 					case MT_EASY:
 						cout << "*********************** 쉬움 ***********************" << endl;
 						break;
@@ -254,7 +320,7 @@ int main() {
 						tPlayer.iExp << endl;
 					cout << "공격력 : " << tPlayer.iAttackMin << " - " <<
 						tPlayer.iAttackMax << "\t방어력 : " << tPlayer.iArmorMin << " - " << tPlayer.iArmorMax << endl;
-					cout << "체력 : " << tPlayer.iHP << " - " <<
+					cout << "체력 : " << tPlayer.iHP << " / " <<
 						tPlayer.iHPMAX << "\t마나 : " << tPlayer.iMP << " - " << tPlayer.iMPMAX << endl;
 					cout << "보유골드 : " << tPlayer.tInventory.iGold << " Gold" << endl;
 
@@ -264,7 +330,7 @@ int main() {
 						tMonster.iLevel << endl;
 					cout << "공격력 : " << tMonster.iAttackMin << " - " <<
 						tMonster.iAttackMax << "\t방어력 : " << tMonster.iArmorMin << " - " << tMonster.iArmorMax << endl;
-					cout << "체력 : " << tMonster.iHP << " - " <<
+					cout << "체력 : " << tMonster.iHP << " / " <<
 						tMonster.iHPMAX << "\t마나 : " << tMonster.iMP << " - " << tMonster.iMPMAX << endl;
 					cout << "획득경험치 : " << tMonster.iExp << "\t획득골드 : " << tMonster.iGoldMin << " - " << tMonster.iGoldMax << endl;
 
@@ -273,7 +339,8 @@ int main() {
 					cout << "메뉴를 선택하세요 : " << endl;
 					cin >> iMenu;
 
-					if (cin.fail()) {
+					if (cin.fail()) 
+					{
 						cin.clear();
 						cin.ignore(1024, '\n');
 						continue;
@@ -282,8 +349,10 @@ int main() {
 
 					else if (iMenu == BATTLE_BACK)
 						break;
-					switch (iMenu) {
-					case BATTLE_ATTACK: {
+					switch (iMenu) 
+					{
+					case BATTLE_ATTACK: 
+					{
 						// 예를 들어 Min 5 Max 15 라고 가정할 경우
 						// 15 - 5 + 1 을 하면 11이 된다. 11로 나눈 나머지는 0~10이
 						// 나오게 되고 여기에 Min값인 5를 더하게 되면
@@ -303,7 +372,8 @@ int main() {
 							"에게 " << iDamage << " 피해를 입혔습니다." << endl;
 
 						// 몬스터가 죽었을 경우를 처리한다.
-						if (tMonster.iHP <= 0) {
+						if (tMonster.iHP <= 0) 
+						{
 							cout << tMonster.strName << " 몬스터가 사망하였습니다." << endl;
 
 							tPlayer.iExp += tMonster.iExp;
@@ -337,7 +407,8 @@ int main() {
 							"에게 " << iDamage << " 피해를 입혔습니다." << endl;
 
 						// 플레이어가 죽었을 경우
-						if (tPlayer.iHP <= 0) {
+						if (tPlayer.iHP <= 0) 
+						{
 							cout << tPlayer.strName << " 플레이어가 사망하였습니다." << endl;
 
 
@@ -353,9 +424,9 @@ int main() {
 							//플레이어의 HP와 MP를 회복한다.
 							tPlayer.iHP = tPlayer.iHPMAX;
 							tPlayer.iMP = tPlayer.iMPMAX;
-							break;
 						}
 
+						system("pause");
 					}
 
 					}
@@ -364,6 +435,39 @@ int main() {
 			}
 			break;
 		case MM_STORE:
+			while (true)
+			{
+				system("cls");
+				cout << "*********************** 상점 ***********************" << endl;
+				cout << "1. 무기상점" << endl;
+				cout << "2. 방어구상점" << endl;
+				cout << "3. 뒤로가기" << endl;
+				cout << "상점을 선택하세요 : ";
+				cin >> iMenu;
+
+				if (cin.fail()) {
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+
+				else if (iMenu == SM_BACK)
+					break;
+
+				switch (iMenu)
+				{
+				case SM_WEAPON:
+					while (true)
+					{
+						system("cls");
+						cout << "*********************** 무기상점 ***********************" << endl;
+						// 판매 목록을 보여준다.
+					}
+					break;
+				case SM_ARMOR:
+					break;
+				}
+			}
 			break;
 		case MM_INVENTORY:
 			break;
